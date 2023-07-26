590a5dd70d4e8f8a8d8163e22ef5c4a65a389670
Nabban muneer pp <nabbanmuneerpp@gmail.com>
2023-07-26 17:16:44 +0530

    admin side

--- src/pages/admin/adminMain.jsx
+++ src/pages/admin/adminMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiMenu } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 const AdminMain = () => {
     const [data, setData] = useState('');
-    
+
     const navigate = useNavigate();
     useEffect(() => {
         axios
@@ -15,9 +15,8 @@ const AdminMain = () => {
             .then((res) => {
                 if (res.status == 201) {
                     setData(res.data);
-                    
-                } else if (res.status == 501)
-                {
+
+                } else if (res.status == 501) {
                     console.log(" Internal Server Error: The server encountered an error while processing the request ");
                 }
             }).catch((error) => {
@@ -43,21 +42,63 @@ const AdminMain = () => {
             </div>
             <div className='grid grid-flow-col justify-around mt-14'>
                 <div className='flex flex-col'>
-                    <div className='text-6xl'>{data.employeeCount}</div>
+                    <div className='text-6xl text-center'>{data.employeeCount}</div>
                     <div className='text-2xl'>Total Employee</div>
                 </div>
                 <div className='flex flex-col'>
-                    <div className='text-6xl'>{data.employerCount}</div>
+                    <div className='text-6xl text-center'>{data.employerCount}</div>
                     <div className='text-2xl'>Total Employer</div>
                 </div>
                 <div className='flex flex-col'>
-                    <div className='text-6xl'>{data.jobCount ? 0 :data.jobCount}</div>
+                    <div className='text-6xl text-center'>{data.jobCount}</div>
                     <div className='text-2xl'>Total Job</div>
                 </div>
                 <div className='flex flex-col'>
-                    <div className='text-6xl'>55</div>
+                    <div className='text-6xl text-center'>55</div>
                     <div className='text-2xl'>total employee</div>
                 </div>
+            </div>
+            <div className='bg-slate-100' >
+                <div className='h-[50px] mt-10  bg-slate-500'>
+                    <div className='text-2xl  ml-5 text-center font-bold'>
+                        Employee Data
+                    </div>
+                </div>
+                {data.employee &&
+                    data.employee.map((value, index) => (
+                        <div className=" m-5  h-[50px] rounded-2xl bg-slate-200  "
+                            onClick={() => selection(value._id)} key={index}>
+                            <div className="font-semibold ml-3 text-xl">{value.userName}</div>
+                        </div>
+                    ))}
+            </div>
+            <div className='bg-slate-100 pb-5' >
+                <div className='h-[50px] mt-10  bg-slate-500'>
+                    <div className='text-2xl  ml-5 text-center font-bold'>
+                        Employer Data
+                    </div>
+                </div>
+                {data.employer &&
+                    data.employer.map((value, index) => (
+                        <div className=" m-5  h-[50px] rounded-2xl bg-slate-200  "
+                            onClick={() => selection(value._id)} key={index}>
+                            <div className="font-semibold ml-3 text-xl">{value.userName}</div>
+                        </div>
+                    ))}
+            </div>
+            <div className='bg-slate-100 pb-5' >
+                <div className='h-[50px] mt-10  bg-slate-500'>
+                    <div className='text-2xl  ml-5 text-center font-bold'>
+                        Jobs
+                    </div>
+                </div>
+                {data.job &&
+                    data.job.map((value, index) => (
+                        <div className=" m-5  h-[50px] rounded-2xl bg-slate-200  "
+                            onClick={() => selection(value._id)} key={index}>
+                            <div className="font-semibold ml-3 text-xl">{value.jobTitle}</div>
+                        </div>
+                    ))}
             </div>
         </div>
     );
